15ff151dda986b28c755097b5c0e43c077e1d7b0
Akulajyosna <jyosnareddy22@gmail.com>
2021-12-29 23:07:31 +0530

    my program 8

---
+++ friday/friday/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace friday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            code o = new code();
+            Console.WriteLine(o.SuccessRate(10));
+            Console.WriteLine(o.productionrateperhour(6));
+            Console.WriteLine(o.workingitemsperminute(6));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ friday/friday/code.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace friday
+{
+    public class code
+    {
+        public static int hours = 221;
+        public double SuccessRate(int speed)
+        {
+            if (speed >= 1 && speed <= 4)
+            {
+                return 1.0;
+            }
+            if (speed >= 5 && speed <= 8)
+            {
+                return 0.9;
+            }
+            if (speed <= 9)
+            {
+                return 0.8;
+            }
+            if (speed == 10)
+            {
+                return 0.77;
+
+            }
+            return speed;
+        }
+        public double productionrateperhour(int speed)
+        {
+            return hours * speed * SuccessRate(speed);
+        }
+        public int workingitemsperminute(int speed)
+        {
+            return (int)productionrateperhour(speed) / 60;
+        }
+
+    }
+}
+

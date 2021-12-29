eef9b0f5207a5c9833ce9084d85912551ecc2a40
Akulajyosna <jyosnareddy22@gmail.com>
2021-12-29 23:10:53 +0530

    my program 9

---
+++ test1/test1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Id\t\tSource\t\tDestination\t\tDate\t\tStatus\t\tNetwork\t\t");
+            FileStream filestreamObj = new FileStream("C:\\capgemini\\assigement1.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(filestreamObj);
+            while (sr.Peek()> 0)
+            {
+                string readmyline = sr.ReadLine();
+                string[] strings = readmyline.Split(':');
+                if (strings.Length > 1)
+                {
+                    string data = strings[1].Split(' ')[0];
+                    Console.Write(strings[1] + "\t\t");
+                    
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+
+
+            }
+        }
+    }
+}

5969f8d9fa2c1161d9ee64722b3bab1ee2c2e2ee
Akulajyosna <jyosnareddy22@gmail.com>
2021-12-29 18:09:03 +0530

    my program 6

---
+++ holiday/holiday/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace holiday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myself s = new myself();
+            s.test(10);
+        }
+    }
+}
---
+++ holiday/holiday/myself.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace holiday//polymorphism-overloading,overriding;
+  //method overloading- creating multiple methods in a class with same names but different parameters;
+{
+    public class myself
+    {
+        public void test()
+        
+        {
+            Console.WriteLine("hi");                                     // test();
+           
+            
+        }
+        public void test(int i)
+        {                                                                
+            Console.WriteLine("capgemini");                             //test(int i);
+        }
+        public void test(int i, string s)
+        {
+            Console.WriteLine("friends");                               ///test( int a,string a);
+        }
+        public void test(string s,int i )
+        {
+            Console.WriteLine("welcome");                              //test( string a,int a); 
+        }
+        public void test(string s)
+        {
+            Console.WriteLine("to all");                             //test(string a) ;   
+        }
+
+
+
+    }
+}

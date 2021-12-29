b7672c1af4ba872d5dc6fd857c15e2cea3b4d8cc
Akulajyosna <jyosnareddy22@gmail.com>
2021-12-29 23:04:31 +0530

    my program 7

---
+++ design/design/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace design
+{
+    class Program
+    {
+        static void Main()
+        {
+            int number;
+            Console.Write("enter number:");
+            number = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0}{0}{0}", number);
+            Console.WriteLine("{0} {0}", number);
+            Console.WriteLine("{0} {0}", number);
+            Console.WriteLine("{0} {0}", number);
+            Console.WriteLine("{0}{0}{0}", number);
+        }
+    }
+}

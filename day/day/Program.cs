257c8353d401539d64df297cbb3f3f43ecabd59d
Akulajyosna <jyosnareddy22@gmail.com>
2021-12-29 17:04:36 +0530

    my program 2

---
+++ day/day/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string revstr = "";
+            string str = Console.ReadLine();            
+                for (int i = str.Length - 1; i >= 0; i--)
+             
+                if(str[i]!='')
+            {
+                revstr = revstr + str[i];
+
+            }
+            Console.WriteLine(revstr);
+            Console.ReadLine();
+
+        }
+        
+    }
+}

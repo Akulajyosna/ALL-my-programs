53fce8a81d8e5242976dd8e7ded63009511cb030
Akulajyosna <jyosnareddy22@gmail.com>
2021-12-29 16:52:41 +0530

    my pogram 1

---
+++ teams1/teams1/Myself.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace teams1
+{
+    class Myself
+    {
+        int rollnumber;
+        string name;
+        string coursejoin;
+        int feepaidfar;
+
+
+        public Myself(int rollnumber, string name, string coursejoin)
+        {
+            this.rollnumber = rollnumber;
+            this.name = name;
+            this.coursejoin = coursejoin;
+        }
+        public void Print()
+        {
+            Console.WriteLine("rollnumber: " +rollnumber);
+            Console.WriteLine("name: " +name);
+            Console.WriteLine("coursejoin: " +coursejoin);
+            Console.WriteLine("feepaidfar: " + feepaidfar);
+        }
+        public void payment(int amount)
+        {
+            feepaidfar = feepaidfar + amount;
+        }
+        public int dueamount
+        {
+            get
+            {
+                return  Totalfee - feepaidfar;
+            }
+        }
+        public int Totalfee
+        {
+            get
+            {
+                return coursejoin == "asp.net" ? 2000 : 3000;
+            }
+
+        }
+    }
+}
+
+        
+
+        
+  
+
+    
+
+
+
+
+    
+
+
+
+    
+
---
+++ teams1/teams1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace teams1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Myself t = new Myself( 01, "krishnasree","asp.net");
+            t.payment(2000);
+            t.Print();
+            Console.WriteLine("dueamount = "+ t.dueamount);
+
+            Myself myselfObj = new Myself(02, " jyoshnasree", "c#");
+            myselfObj.payment(1500);
+            myselfObj.Print();
+            Console.WriteLine("dueamount= " + myselfObj.dueamount);
+        }
+    }
+}

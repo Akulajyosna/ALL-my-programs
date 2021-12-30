98f4dd88115b45a85a8d3ce329fdce76ba4721da
Akulajyosna <jyosnareddy22@gmail.com>
2021-12-30 08:45:40 +0530

    my program 11

---
+++ mul/mul/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace mul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+            Console.Write("num1:");
+            num1 = int.Parse(Console.ReadLine());
+            Console.Write("num2:");
+            num2 = int.Parse(Console.ReadLine());
+            Console.Write("num3:");
+            num3 = int.Parse(Console.ReadLine());
+            int result = ((num1+num2)*num3);
+            int result2 = num1 * num2 + num2 * num3;
+            Console.WriteLine("s:" +result2);
+            Console.WriteLine("s" +result);
+        }
+    }
+}

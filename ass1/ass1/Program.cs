81fe47c0345154677808ef017b25b229cb64c129
Akulajyosna <jyosnareddy22@gmail.com>
2021-12-30 08:48:27 +0530

    my program 11

---
+++ ass1/ass1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace ass1
+{
+       class Program
+       {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("C:\\capgemini\\assigement1.txt", FileMode.Create, FileAccess.Write);
+            StreamReader sr = new StreamReader(fs);
+            while (sr.Peek() > 0)
+            {
+                string readmyline = sr.ReadLine();
+                string[] strings = readmyline.Split(':');
+                if (strings.Length > 1)
+                    Console.WriteLine(strings[1]);
+
+
+            }
+        }
+    }
+}

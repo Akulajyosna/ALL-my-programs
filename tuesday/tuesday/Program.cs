a5f17970c60b988f06c9777fff25b75abc911db6
Akulajyosna <jyosnareddy22@gmail.com>
2021-12-30 09:13:41 +0530

    my program 15

---
+++ tuesday/tuesday/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+
+
+namespace tuesday
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+
+            ArrayList list = new ArrayList();
+
+            int total = 3;
+            for (int i = 0; i < total; i++)
+            {
+                string name = Console.ReadLine();
+                int age = int.Parse(Console.ReadLine());
+
+                list.Add(new person()
+                {
+                    Name = name,
+                    Age = age
+                });
+            }
+
+            foreach (person p in list)
+            {
+                Console.WriteLine(p.ToString());
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ tuesday/tuesday/person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+
+namespace tuesday
+{
+    class person
+    {
+ 
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+        public override string ToString()
+        {
+            return Name + " - " + Age;
+        }
+
+
+    }
+}
+

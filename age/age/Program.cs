f6594cf4a483df081d1f976a48e3f371da552ab0
Akulajyosna <jyosnareddy22@gmail.com>
2021-12-30 12:15:41 +0530

    my program 16

---
+++ age/age/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace age
+{
+    class Program
+    {
+        static void Main()
+        {
+            int age;
+            Console.Write("age:");
+            age = Convert.ToInt32(Console.ReadLine());
+            if (age < 25)
+            {
+                Console.WriteLine("you are young");
+
+            }
+            else
+            {
+                Console.WriteLine("you are old");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}

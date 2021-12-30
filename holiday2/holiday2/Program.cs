0f8aa7d9e5a97e01e1c65189f532ddf1f680d96b
Akulajyosna <jyosnareddy22@gmail.com>
2021-12-30 09:04:49 +0530

    my program 14

---
+++ holiday2/holiday2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace holiday2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            small s = new small();
+            s.show();
+            s.test();
+               
+        }
+    }
+}
---
+++ holiday2/holiday2/big.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace holiday2//method overriding- having two methods with same name and same parameters
+                  //one should be in the base class and other method should be in a child class
+                  //Virtual and Override keywords are used to achieve method overriding.//
+{
+    public class big
+    {
+        public  virtual void show()
+        {
+            Console.WriteLine("my name is mayareddy");
+        }
+        public void test()
+        {
+            Console.WriteLine("i am from ap");
+        }
+    }
+    public  class small:big
+    {
+        public override   void show()
+       {
+           Console.WriteLine(" hi all");
+        }
+
+    }
+
+}

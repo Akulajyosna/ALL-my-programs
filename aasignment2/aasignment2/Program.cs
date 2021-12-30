eecdfb4858b75f78471beab6140d7d808090bdb3
Akulajyosna <jyosnareddy22@gmail.com>
2021-12-30 12:57:35 +0530

    my program 17

---
+++ aasignment2/aasignment2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace aasignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter your string : ");
+            string[] s = Console.ReadLine().Split(' ');
+
+            string ans = "";
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                ans += s[i] + " ";
+            }
+            Console.Write("Reversed String:\n");
+            Console.Write(ans.Substring(0,
+                                        ans.Length - 1));
+        }
+    }
+}

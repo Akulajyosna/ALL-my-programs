843e1327a948c804bb81115961c1ed7fd35ef7b2
Akulajyosna <jyosnareddy22@gmail.com>
2021-12-30 12:37:00 +0530

    my program 17

---
+++ assignment/assignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace assignment
+{
+    class Program
+    {
+        public static void Main()
+            {
+            String str = Console.ReadLine();
+                SubString(str, str.Length);
+            }
+      
+        public static void SubString(String str, int n)
+        {
+            for (int i = 0; i < n; i++)
+                for (int j = 1; j <= n - i; j++)
+                    Console.WriteLine(str.Substring(i, j));
+        }
+
+
+
+    }
+}

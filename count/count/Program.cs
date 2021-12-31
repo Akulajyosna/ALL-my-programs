d7f7f15fd5018f057ebfe29aba1bb1f9389010f1
Akulajyosna <jyosnareddy22@gmail.com>
2021-12-31 15:49:11 +0530

    my program 18

---
+++ count/count/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace count
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {
+            string str;
+             Console.Write("string=");
+            str = Console.ReadLine();
+                while (str.Length > 0)
+                {
+                    Console.Write(str[0] + " = ");
+                    int cal = 0;
+                    for (int j = 0; j < str.Length; j++)
+                    {
+                        if (str[0] == str[j])
+                        {
+                            cal++;
+                        }
+                    }
+                    Console.WriteLine(cal);
+                    str = str.Replace(str[0].ToString(), string.Empty);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+

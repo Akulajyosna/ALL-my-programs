9b0b3275a7f9d07e7bbf365d464db8a236702b1d
Akulajyosna <jyosnareddy22@gmail.com>
2022-01-10 17:14:43 +0530

    created tables HRDB

--- creating tables HRDB.sql
+++ creating tables HRDB.sql
@@ -1,11 +1,19 @@
 create database HRDB
 drop table regions 
 create table regions(region_id int primary key,region_name varchar(25))
+select * from regions
 drop table countries
 create table countries(country_id char(2) primary key,country_name varchar(40),region_id int references regions(region_id))
+select  * from countries
 create table locations(location_id int primary key,street_address varchar(25),postal_code varchar(15),city varchar(30),state_province varchar(12),country_id char(2) references countries(country_id))
+select * from locations
 create table department(department_id int primary key,deparment_name varchar(30),manager_id int,location_id int references locations(location_id))
+select  * from  department
 create table job_history(employee_id int primary key,start_date date,end_date date,job_id varchar(10) references jobs(job_id),department_id int references department(department_id))
+select * from job_history
 create table employees(employee_id int references job_history(employee_id),first_name varchar(20),last_name varchar(25),email varchar(25),phone_number varchar(20),hire_date date,job_id varchar(10) references jobs(job_id),salary int,commission_pct int,manager_id int,department_id int references department(department_id))
+select * from employees
 create table jobs(job_id varchar(10) primary key,job_title varchar(35),min_salary int,max_salary int)
+select * from jobs
 create table job_grades(grade_level varchar(2) primary key,lowest_sal int,highest_sal int)
+select * from job_grades
